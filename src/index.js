742ac6759af3c440004191992e415ee803d12446
JPSchellenberg <jps@Lumi.education>
2019-06-25 14:04:20 +0200

    Fix invalid uberName in getLibraryOverview (fixes #28)

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ class H5PEditor {
                     .loadLibrary(machineName, majorVersion, minorVersion)
                     .then(library => {
                         return {
-                            uberName: `${library.machineName}-${
+                            uberName: `${library.machineName} ${
                                 library.majorVersion
                             }.${library.minorVersion}`,
                             name: library.machineName,
