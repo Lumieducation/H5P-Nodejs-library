80f30d34c4ce5dd0769d980fb34b7b3c214c9c2c
Sebastian Rettig <sr258@users.noreply.github.com>
2019-11-04 12:37:55 +0100

    docs(h5pcore): add documentation on core updates (#230)

--- src/H5PEditor.ts
+++ src/H5PEditor.ts
@@ -472,6 +472,10 @@ export default class H5PEditor {
         return {
             ...defaultEditorIntegration,
             ajaxPath: this.ajaxPath,
+            apiVersion: {
+                majorVersion: this.config.coreApiVersion.major,
+                minorVersion: this.config.coreApiVersion.minor
+            },
             assets: {
                 css: [
                     '/core/styles/h5p.css',
