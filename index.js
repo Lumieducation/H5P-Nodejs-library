36f23c7579093a8a176e7ed2568132beb810fb53
JPSchellenberg <jps@Lumi.education>
2019-11-08 13:02:51 +0100

    feat(types): add types to released module

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Editor: require('./build/src/H5PEditor').default,
-    englishStrings: require('./build/src/translations/translations/en.json'),
-    LibraryName: require('./build/src/LibraryName').default,
-    PackageExporter: require('./build/src/PackageExporter').default,
-    Player: require('./build/src/H5PPlayer').default,
-    TranslationService: require('./build/src/TranslationService').default
-};
