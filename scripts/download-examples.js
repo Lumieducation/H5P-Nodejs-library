4a35036ddaf7f3479ad9c4210d6a3442fc11357b
Sebastian Rettig <serettig@posteo.de>
2019-12-03 18:24:23 +0100

    improved H5P content download script

--- scripts/download-examples.js
+++ scripts/download-examples.js
@@ -8,56 +8,79 @@ const path = require('path');
  * @param {string} directoryPath Path to a directory on the disk to save the downloaded H5P to.
  */
 const downloadH5pPackages = async (contentTypeCacheFilePath, directoryPath) => {
-    const machineNames = (await fs.readJSON(contentTypeCacheFilePath)).contentTypes.map(ct => ct.id);
+    const machineNames = (
+        await fs.readJSON(contentTypeCacheFilePath)
+    ).contentTypes.map(ct => ct.id);
 
-    console.log(`Found ${machineNames.length} packages.`);
+    console.log(`Found ${machineNames.length} already downloaded packages.`);
 
     // Create the directory if it doesn't exist
     if (!(await fs.pathExists(directoryPath))) {
-        await fs.mkdir(directoryPath)
+        await fs.mkdir(directoryPath);
     }
 
     // Counts how many downloads have been finished
     let downloadsFinished = 0;
 
     // Promise.all allows parallel downloads
-    await Promise.all(
+    return Promise.all(
         machineNames
-            .filter(machineName => machineName !== "H5P.IFrameEmbed") // IFrameEmbed is broken and is deprecated
+            .filter(machineName => machineName !== 'H5P.IFrameEmbed') // IFrameEmbed is broken and is deprecated
             .filter(machineName => {
-                if (fs.pathExistsSync(path.join(directoryPath, `${machineName}.h5p`))) {
+                if (
+                    fs.pathExistsSync(
+                        path.join(directoryPath, `${machineName}.h5p`)
+                    )
+                ) {
                     downloadsFinished += 1;
-                    console.log(`${downloadsFinished}/${machineNames.length} ${machineName}.h5p has already been downloaded. Skipping!`);
+                    console.log(
+                        `${downloadsFinished}/${machineNames.length} ${machineName}.h5p has already been downloaded. Skipping!`
+                    );
                     return false;
                 }
                 return true;
             })
-            .map(contentType => axios.default.get(`http://api.h5p.org/v1/content-types/${contentType}`, { responseType: "stream" }).then(response => {
-                return new Promise((resolve) => {
-                    const file = fs.createWriteStream(`${directoryPath}/${contentType}.h5p`);
-                    file.on("finish", () => {
-                        downloadsFinished += 1;
-                        console.log(`Downloaded example ${downloadsFinished}/${machineNames.length}: ${contentType}.h5p`);
-                        resolve(`${contentType}.h5p`);
+            .map(contentType =>
+                axios.default
+                    .get(`http://api.h5p.org/v1/content-types/${contentType}`, {
+                        responseType: 'stream'
                     })
-                    response.data.pipe(file);
-                })
-            }).catch(error => {
-                downloadsFinished += 1;
-                console.error(`${downloadsFinished}/${machineNames.length} Error downloading ${contentType}: ${error.response.status} ${error.response.statusText}`);
-                return Promise.resolve();
-            })
-            ));
-}
+                    .then(response => {
+                        return new Promise(resolve => {
+                            const file = fs.createWriteStream(
+                                `${directoryPath}/${contentType}.h5p`
+                            );
+                            file.on('finish', () => {
+                                downloadsFinished += 1;
+                                console.log(
+                                    `Downloaded example ${downloadsFinished}/${machineNames.length}: ${contentType}.h5p`
+                                );
+                                resolve(`${contentType}.h5p`);
+                            });
+                            response.data.pipe(file);
+                        });
+                    })
+                    .catch(error => {
+                        downloadsFinished += 1;
+                        const errorMessage = `${downloadsFinished}/${machineNames.length} Error downloading ${contentType}: ${error.response.status} ${error.response.statusText}`;
+                        console.error(errorMessage);
+                        return Promise.reject(errorMessage);
+                    })
+            )
+    );
+};
 
 const contentTypeCacheFile = path.resolve(process.argv[2]);
 const directory = path.resolve(process.argv[3]);
 
-console.log("Downloading content type examples from H5P Hub.");
+console.log('Downloading content type examples from H5P Hub.');
 console.log(`Using content types from ${contentTypeCacheFile}`);
 console.log(`Downloading to ${directory}`);
 
 downloadH5pPackages(contentTypeCacheFile, directory)
-    .then(() => {
-        console.log("Download finished!");
+    .then(files => {
+        console.log(`Download finished! Downloaded ${files.length} files.`);
+    })
+    .catch(error => {
+        process.exit(1);
     });
