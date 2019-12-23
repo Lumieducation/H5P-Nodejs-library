12de4bb4fc2e40988b7be5d7dec27cca8c8efa90
Sebastian Rettig <sr258@users.noreply.github.com>
2019-12-23 17:53:23 +0100

    refactor(test): removed redundant variables from H5PPlayer test of integration

--- test/H5PPlayer.integration.test.ts
+++ test/H5PPlayer.integration.test.ts
@@ -2,10 +2,6 @@ import H5PPlayer from '../src/H5PPlayer';
 
 describe('H5PPlayer.generateIntegration()', () => {
     it('should use the passed integration', () => {
-        const contentId = 'foo';
-        const contentObject = {};
-        const h5pObject = {};
-
         const integration = new H5PPlayer(
             undefined,
             undefined,
