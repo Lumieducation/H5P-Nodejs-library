18e283a3b1f0ca333910daf0dcd8948190a69679
Sebastian Rettig <serettig@posteo.de>
2020-02-11 11:29:06 +0100

    refactor(temporary storage): Cleaned up code

--- src/implementation/fs/DirectoryTemporaryFileStorage.ts
+++ src/implementation/fs/DirectoryTemporaryFileStorage.ts
@@ -25,34 +25,33 @@ export default class DirectoryTemporaryFileStorage
     constructor(private directory: string) {}
 
     public async deleteFile(filename: string, userId: string): Promise<void> {
-        await fsExtra.remove(path.join(this.directory, userId, filename));
-        await fsExtra.remove(
-            `${path.join(this.directory, userId, filename)}.metadata`
-        );
-        const userFiles = await fsExtra.readdir(
-            path.join(this.directory, userId)
-        );
+        const filePath = this.getAbsoluteFilePath(userId, filename);
+        await fsExtra.remove(filePath);
+        await fsExtra.remove(`${filePath}.metadata`);
+
+        const directoryPath = this.getAbsoluteUserDirectoryPath(userId);
+        const userFiles = await fsExtra.readdir(directoryPath);
         if (userFiles.length === 0) {
-            await fsExtra.rmdir(path.join(this.directory, userId));
+            await fsExtra.rmdir(directoryPath);
         }
     }
 
     public async fileExists(filename: string, user: IUser): Promise<boolean> {
-        const absoluteFilename = path.join(this.directory, user.id, filename);
-        return fsExtra.pathExists(absoluteFilename);
+        const filePath = this.getAbsoluteFilePath(user.id, filename);
+        return fsExtra.pathExists(filePath);
     }
 
     public async getFileStream(
         filename: string,
         user: IUser
     ): Promise<ReadStream> {
-        const absoluteFilename = path.join(this.directory, user.id, filename);
-        if (!(await fsExtra.pathExists(absoluteFilename))) {
+        const filePath = this.getAbsoluteFilePath(user.id, filename);
+        if (!(await fsExtra.pathExists(filePath))) {
             throw new H5pError(
                 `The file ${filename} is not accessible for user ${user.id} or does not exist.`
             );
         }
-        return fsExtra.createReadStream(absoluteFilename);
+        return fsExtra.createReadStream(filePath);
     }
 
     public async listFiles(user?: IUser): Promise<ITemporaryFile[]> {
@@ -61,7 +60,7 @@ export default class DirectoryTemporaryFileStorage
             await Promise.all(
                 users.map(async u => {
                     const filesOfUser = await fsExtra.readdir(
-                        path.join(this.directory, u)
+                        this.getAbsoluteUserDirectoryPath(u)
                     );
                     return Promise.all(
                         filesOfUser
@@ -79,11 +78,11 @@ export default class DirectoryTemporaryFileStorage
         user: IUser,
         expirationTime: Date
     ): Promise<ITemporaryFile> {
-        await fsExtra.ensureDir(path.join(this.directory, user.id));
-        const absoluteFilename = path.join(this.directory, user.id, filename);
-        const writeStream = fsExtra.createWriteStream(absoluteFilename);
+        await fsExtra.ensureDir(this.getAbsoluteUserDirectoryPath(user.id));
+        const filePath = this.getAbsoluteFilePath(user.id, filename);
+        const writeStream = fsExtra.createWriteStream(filePath);
         await promisepipe(dataStream, writeStream);
-        await fsExtra.writeJSON(`${absoluteFilename}.metadata`, {
+        await fsExtra.writeJSON(`${filePath}.metadata`, {
             expiresAt: expirationTime.getTime()
         });
         return {
@@ -93,12 +92,20 @@ export default class DirectoryTemporaryFileStorage
         };
     }
 
+    private getAbsoluteFilePath(userId: string, filename: string): string {
+        return path.join(this.directory, userId, filename);
+    }
+
+    private getAbsoluteUserDirectoryPath(userId: string): string {
+        return path.join(this.directory, userId);
+    }
+
     private async getTemporaryFileInfo(
         filename: string,
         userId: string
     ): Promise<ITemporaryFile> {
         const metadata = await fsExtra.readJSON(
-            `${path.join(this.directory, userId, filename)}.metadata`
+            `${this.getAbsoluteFilePath(userId, filename)}.metadata`
         );
         return {
             expiresAt: new Date(metadata.expiresAt),
