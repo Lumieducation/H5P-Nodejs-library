62f8d4176de5a01ae157a0730bd872658fcdce43
Sebastian Rettig <sr258@users.noreply.github.com>
2020-05-22 16:29:59 +0200

    refactor(e2e): cleaned up and extended e2e tests (#528)

--- test/e2e/H5PPlayer.DisplayContent.test.ts
+++ test/e2e/H5PPlayer.DisplayContent.test.ts
@@ -1,137 +1,54 @@
-// tslint:disable-next-line: no-implicit-dependencies
-import express from 'express';
-import fs from 'fs';
+import fsExtra from 'fs-extra';
 import path from 'path';
-import promiseQueue from 'promise-queue';
 import puppeteer from 'puppeteer';
 
-import H5PPlayer from '../../src/H5PPlayer';
-import H5PConfig from '../../src/implementation/H5PConfig';
-import PackageImporter from '../../src/PackageImporter';
-import { ILibraryName } from '../../src';
+import exportPackage from './helpers/exportPackage';
 
-const contentPath = `${path.resolve('')}/test/data/hub-content`;
-const extractedContentPath = `${path.resolve(
-    ''
-)}/test/data/hub-content-extracted/`;
+describe('e2e test: play content', () => {
+    const examplesPath = path.resolve('test/data/hub-content');
+    const contentPath = path.resolve('h5p/content');
+    const librariesPath = path.resolve('h5p/libraries');
 
-// TODO: Replace console.log with logger.
+    let browser: puppeteer.Browser;
+    let page: puppeteer.Page;
 
-fs.readdir(contentPath, (fsError, files) => {
-    Promise.all(
-        files.map((file) => {
-            // tslint:disable-next-line: no-console
-            console.log(`extracting: ${file}`);
-            return PackageImporter.extractPackage(
-                `${contentPath}/${file}`,
-                `${extractedContentPath}/${file}`,
-                {
-                    includeContent: true,
-                    includeLibraries: true,
-                    includeMetadata: true
-                }
-            );
-        })
-    ).then(() => {
-        const server = express();
-        const queue = new promiseQueue(3, Infinity);
-        server.use('/h5p/core', express.static(`${path.resolve('')}/h5p/core`));
-        server.get('/examples/:name', (req, res) => {
-            const { name } = req.params;
-            const dir = `${extractedContentPath}/${name}`;
-            server.use('/h5p/libraries', express.static(dir));
-            server.use(
-                `/h5p/content/${name}`,
-                express.static(`${dir}/content`)
-            );
-
-            const mockLibraryStorage: any = {
-                getLibrary: async (libName: ILibraryName) => {
-                    return require(`${dir}/${libName.machineName}-${libName.majorVersion}.${libName.minorVersion}/library.json`);
-                }
-            };
-            const h5pObject = require(`${dir}/h5p.json`);
-            const contentObject = require(`${dir}/content/content.json`);
-            return new H5PPlayer(
-                mockLibraryStorage,
-                undefined,
-                new H5PConfig(undefined),
-                undefined,
-                undefined
-            )
-                .render(name, contentObject, h5pObject)
-                .then((h5pPage) => res.end(h5pPage))
-                .catch((error) => res.status(500).end(error.message));
-        });
-        server.use((error, req, res, next) => {
-            if (error) {
-                // tslint:disable-next-line: no-console
-                console.log(error);
-                process.exit(1);
-            }
-        });
-        server.listen(8080, () => {
-            // tslint:disable-next-line: no-console
-            console.log(`server running on http://localhost: ${8080}`);
-            puppeteer.launch({ devtools: true }).then((browser) => {
-                fs.readdir(
-                    extractedContentPath,
-                    (fsExtractedError, extractedFiles) => {
-                        extractedFiles.forEach((file, index) => {
-                            queue
-                                .add(
-                                    () =>
-                                        new Promise((resolve, reject) => {
-                                            browser.newPage().then((page) => {
-                                                page.on('pageerror', (msg) => {
-                                                    // tslint:disable-next-line: no-console
-                                                    console.log(
-                                                        `${file}: ERROR`,
-                                                        msg
-                                                    );
-                                                    process.exit(1);
-                                                    reject(
-                                                        new Error(
-                                                            JSON.stringify(file)
-                                                        )
-                                                    );
-                                                });
-                                                page.goto(
-                                                    `http://localhost:8080/examples/${file}`,
-                                                    {
-                                                        waitUntil:
-                                                            'networkidle0'
-                                                    }
-                                                ).then(() => {
-                                                    setTimeout(() => {
-                                                        page.close();
-                                                        // tslint:disable-next-line: no-console
-                                                        console.log(
-                                                            `${file}: OK`
-                                                        );
-                                                        resolve();
-                                                    }, 1000);
-                                                });
-                                            });
-                                        })
-                                )
-                                .catch((error) => {
-                                    // tslint:disable-next-line: no-console
-                                    console.log(error);
-                                    process.exit(1);
-                                });
-                        });
-                    }
-                );
-
-                const interval = setInterval(() => {
-                    if (queue.getPendingLength() === 0) {
-                        browser.close();
-                        clearInterval(interval);
-                        process.exit(0);
-                    }
-                }, 500);
-            });
-        });
+    beforeAll(async () => {
+        browser = await puppeteer.launch({});
     });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        page = await browser.newPage();
+    });
+
+    afterEach(async () => {
+        await page.close();
+    });
+
+    for (const file of fsExtra.readdirSync(examplesPath)) {
+        it(`playing ${file}`, async () => {
+            const contentId = await exportPackage(
+                path.join(examplesPath, file),
+                librariesPath,
+                contentPath,
+                {
+                    clearDirs: true
+                }
+            );
+
+            const errorHandler = jest.fn();
+            page.on('pageerror', (error) => {
+                // tslint:disable-next-line: no-console
+                console.error(`Error when playing ${file}: ${error}`);
+                errorHandler();
+            });
+            await page.goto(`http://localhost:8080/h5p/play/${contentId}`, {
+                waitUntil: ['networkidle0', 'load']
+            });
+            expect(errorHandler).not.toHaveBeenCalled();
+        }, 10000);
+    }
 });
