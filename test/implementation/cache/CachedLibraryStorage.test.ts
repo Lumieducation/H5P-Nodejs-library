a1b9cab0f7e6183968557bac186fca5aa185fe3e
Sebastian Rettig <sr258@users.noreply.github.com>
2020-08-28 09:57:45 +0200

    test(caching): added and improved tests

--- test/implementation/cache/CachedLibraryStorage.test.ts
+++ test/implementation/cache/CachedLibraryStorage.test.ts
@@ -1,59 +1,175 @@
 import path from 'path';
 import cacheManager from 'cache-manager';
+import fsExtra from 'fs-extra';
 
 import { ILibraryStorage } from '../../../src/types';
 import CachedLibraryStorage from '../../../src/implementation/cache/CachedLibraryStorage';
 import FileLibraryStorage from '../../../src/implementation/fs/FileLibraryStorage';
 
+type FunctionPropertyNames<T> = {
+    [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+}[keyof T] &
+    string;
+
 describe('CachedLibraryStorage', () => {
-    let cachedStorage: ILibraryStorage;
-    let storage: ILibraryStorage;
+    let defaultCachedStorage: ILibraryStorage;
+    let defaultUncachedStorage: ILibraryStorage;
+    let defaultCheckIfFunctionCaches;
+
     beforeEach(() => {
-        storage = new FileLibraryStorage(path.resolve('test/data/libraries'));
-        cachedStorage = new CachedLibraryStorage(
-            storage,
+        defaultUncachedStorage = new FileLibraryStorage(
+            path.resolve('test/data/libraries')
+        );
+        defaultCachedStorage = new CachedLibraryStorage(
+            defaultUncachedStorage,
             cacheManager.caching({
                 store: 'memory',
                 ttl: 60 * 60 * 24,
                 max: 2 ** 10
             })
         );
+        defaultCheckIfFunctionCaches = checkIfFunctionCaches(
+            defaultUncachedStorage,
+            defaultCachedStorage
+        );
     });
 
+    /**
+     * Calls a function on the cached storage object twice. If expectedResult is
+     * defined, asserts that the result returned both times is the one in
+     * expectedResult.
+     * Works with async and sync functions.
+     * @param functionName the function to call
+     * @param expectedResult the expected result
+     * @param functionParameters the parameters to pass to the function
+     * @returns the number of times the un-cached function was called
+     */
+    const checkIfFunctionCaches = (uncachedStorage, cachedStorage) => async <
+        M extends FunctionPropertyNames<Required<ILibraryStorage>>
+    >(
+        functionName: M,
+        expectedResult: any,
+        ...functionParameters: any[]
+    ): Promise<number> => {
+        const spy = jest.spyOn(uncachedStorage, functionName);
+        const result1 = await cachedStorage[functionName].call(
+            cachedStorage,
+            ...functionParameters
+        );
+        if (expectedResult) {
+            if (typeof expectedResult === 'object') {
+                expect(result1).toMatchObject(expectedResult);
+            } else {
+                expect(result1).toEqual(expectedResult);
+            }
+        }
+        const result2 = await cachedStorage[functionName].call(
+            cachedStorage,
+            ...functionParameters
+        );
+        if (expectedResult) {
+            if (typeof expectedResult === 'object') {
+                expect(result1).toMatchObject(result2);
+            } else {
+                expect(result1).toEqual(result2);
+            }
+        }
+        return spy.mock.calls.length;
+    };
+
     it("doesn't read from the wrapped storage for metadata twice", async () => {
-        const spy = jest.spyOn(storage, 'getLibrary');
-        const metadata1 = await cachedStorage.getLibrary({
-            machineName: 'H5P.Example1',
-            majorVersion: 1,
-            minorVersion: 1
-        });
-        expect(metadata1).toMatchObject({
-            title: 'Example 1',
-            description: 'The description of content type 1',
-            majorVersion: 1,
-            minorVersion: 1,
-            patchVersion: 1,
-            runnable: 1,
-            author: 'H5P NodeJs Implementation',
-            license: 'MIT',
-            machineName: 'H5P.Example1',
-            preloadedJs: [
+        expect(
+            await defaultCheckIfFunctionCaches(
+                'getLibrary',
+                await fsExtra.readJSON(
+                    path.resolve(
+                        'test/data/libraries/H5P.Example1-1.1/library.json'
+                    )
+                ),
                 {
-                    path: 'greetingcard.js'
+                    machineName: 'H5P.Example1',
+                    majorVersion: 1,
+                    minorVersion: 1
                 }
-            ],
-            preloadedCss: [
+            )
+        ).toEqual(1);
+    });
+
+    it("doesn't read from the wrapped storage for semantics twice (as JSON)", async () => {
+        expect(
+            await defaultCheckIfFunctionCaches(
+                'getFileAsJson',
+                await fsExtra.readJSON(
+                    path.resolve(
+                        'test/data/libraries/H5P.Example1-1.1/semantics.json'
+                    )
+                ),
                 {
-                    path: 'greetingcard.css'
+                    machineName: 'H5P.Example1',
+                    majorVersion: 1,
+                    minorVersion: 1
+                },
+                'semantics.json'
+            )
+        ).toEqual(1);
+    });
+
+    it("doesn't read from the wrapped storage for semantics twice (as string)", async () => {
+        expect(
+            await defaultCheckIfFunctionCaches(
+                'getFileAsString',
+                (
+                    await fsExtra.readFile(
+                        path.resolve(
+                            'test/data/libraries/H5P.Example1-1.1/semantics.json'
+                        )
+                    )
+                ).toString(),
+                {
+                    machineName: 'H5P.Example1',
+                    majorVersion: 1,
+                    minorVersion: 1
+                },
+                'semantics.json'
+            )
+        ).toEqual(1);
+    });
+
+    it('reads from the wrapped storage for uncached files twice (as string)', async () => {
+        expect(
+            await defaultCheckIfFunctionCaches(
+                'getFileAsString',
+                (
+                    await fsExtra.readFile(
+                        path.resolve(
+                            'test/data/libraries/H5P.Example1-1.1/greetingcard.js'
+                        )
+                    )
+                ).toString(),
+                {
+                    machineName: 'H5P.Example1',
+                    majorVersion: 1,
+                    minorVersion: 1
+                },
+                'greetingcard.js'
+            )
+        ).toEqual(2);
+    });
+
+    it('caches getInstalledLibraryNames', async () => {
+        expect(
+            await defaultCheckIfFunctionCaches('getInstalledLibraryNames', [
+                {
+                    machineName: 'H5P.Example1',
+                    majorVersion: 1,
+                    minorVersion: 1
+                },
+                {
+                    machineName: 'H5P.Example3',
+                    majorVersion: 2,
+                    minorVersion: 1
                 }
-            ]
-        });
-        const metadata2 = await cachedStorage.getLibrary({
-            machineName: 'H5P.Example1',
-            majorVersion: 1,
-            minorVersion: 1
-        });
-        expect(metadata1).toMatchObject(metadata2);
-        expect(spy).toHaveBeenCalledTimes(1);
+            ])
+        ).toEqual(1);
     });
 });
