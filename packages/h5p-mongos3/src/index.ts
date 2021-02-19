b25dbaaaf4db02cca59fd8dc4330f86d52b96708
Sebastian Rettig <sr258@users.noreply.github.com>
2021-02-19 22:52:19 +0100

    fix(h5p-mongos3): fixed default export (#1070)
    Closes h5p-mongos3: incorrect export #1069

--- packages/h5p-mongos3/src/index.ts
+++ packages/h5p-mongos3/src/index.ts
@@ -3,9 +3,4 @@ import initS3 from './initS3';
 import initMongo from './initMongo';
 import S3TemporaryFileStorage from './S3TemporaryFileStorage';
 
-export default {
-    MongoS3ContentStorage,
-    initS3,
-    initMongo,
-    S3TemporaryFileStorage
-};
+export { MongoS3ContentStorage, initS3, initMongo, S3TemporaryFileStorage };
