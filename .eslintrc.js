551838ead3baeca9199c293c4ac670101ff5c5bf
Sebastian Rettig <serettig@posteo.de>
2021-03-09 09:54:15 +0100

    refactor(all): try to fix rest example client linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
         browser: true,
         node: true
     },
+    root: true,
     extends: [
         //'eslint:recommended',
         //'plugin:@typescript-eslint/recommended',
--- packages/h5p-rest-example-client/.eslintrc.js
+++ packages/h5p-rest-example-client/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true
     },
+    root: true,
     extends: ['react-app', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
