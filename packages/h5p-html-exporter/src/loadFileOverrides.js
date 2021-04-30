17d194118ab3059fdba4c47bd75b733d95b60e56
Sebastian Rettig <serettig@posteo.de>
2021-04-30 12:10:47 +0200

    refactor(h5p-html-exporter): load file override encapsulated

--- packages/h5p-html-exporter/src/loadFileOverrides.js
+++ packages/h5p-html-exporter/src/loadFileOverrides.js
@@ -3,60 +3,64 @@
  * any files in the library directories which weren't already included.
  */
 
-// By providing a custom implementation of H5P.ContentType we can inject the 
-// resource files when a H5P content type generates the path to a file.
-H5P.ContentType = function (isRootLibrary) {
-    function ContentType() {}
-    ContentType.prototype = new H5P.EventDispatcher();
-    ContentType.prototype.isRoot = function () {
-        return isRootLibrary;
+(function () {
+    // By providing a custom implementation of H5P.ContentType we can inject the
+    // resource files when a H5P content type generates the path to a file.
+    H5P.ContentType = function (isRootLibrary) {
+        function ContentType() {}
+        ContentType.prototype = new H5P.EventDispatcher();
+        ContentType.prototype.isRoot = function () {
+            return isRootLibrary;
+        };
+        ContentType.prototype.getLibraryFilePath = function (filePath) {
+            return (
+                furtherH5PInlineResources[
+                    this.libraryInfo.versionedNameNoSpaces + '/' + filePath
+                ] || filePath
+            );
+        };
+        return ContentType;
     };
-    ContentType.prototype.getLibraryFilePath = function (filePath) {
-        return furtherH5PInlineResources[
-            this.libraryInfo.versionedNameNoSpaces + '/' + filePath
-        ] || filePath;
-    };
-    return ContentType;
-};
 
-// We override setting <script src="..."/> behavior to be able to inject scripts 
-// that don't use H5P.ContentType.getLibraryFilePath.
-nativeSetScriptAttribute = HTMLScriptElement.prototype.setAttribute;
-HTMLScriptElement.prototype.setAttribute = function (name, value) {
-    if (name === 'src') {
-        if (value.startsWith('./libraries/')) {
-            const file = value.substr(12);
-            if (furtherH5PInlineResources[file]) {
-                value = furtherH5PInlineResources[file];
+    // We override setting <script src="..."/> behavior to be able to inject scripts
+    // that don't use H5P.ContentType.getLibraryFilePath.
+    var nativeSetScriptAttribute = HTMLScriptElement.prototype.setAttribute;
+    HTMLScriptElement.prototype.setAttribute = function (name, value) {
+        if (name === 'src') {
+            if (value.startsWith('./libraries/')) {
+                const file = value.substr(12);
+                if (furtherH5PInlineResources[file]) {
+                    value = furtherH5PInlineResources[file];
+                }
             }
         }
-    }
-    nativeSetScriptAttribute.call(this, name, value);
-};
-Object.defineProperty(HTMLScriptElement.prototype, 'src', {
-    // also apply the modification if the src is set with the src property
-    set(value) {
-        this.setAttribute('src', value);
-    }
-});
+        nativeSetScriptAttribute.call(this, name, value);
+    };
+    Object.defineProperty(HTMLScriptElement.prototype, 'src', {
+        // also apply the modification if the src is set with the src property
+        set(value) {
+            this.setAttribute('src', value);
+        }
+    });
 
-// We override setting <link href="..."/> to be able to inject styles that don't
-// use H5P.ContentType.getLibraryFilePath.
-nativeSetLinkAttribute = HTMLLinkElement.prototype.setAttribute;
-HTMLLinkElement.prototype.setAttribute = function (name, value) {
-    if (name === 'href') {
-        if (value.startsWith('./libraries/')) {
-            const file = value.substr(12);
-            if (furtherH5PInlineResources[file]) {
-                value = furtherH5PInlineResources[file];
+    // We override setting <link href="..."/> to be able to inject styles that don't
+    // use H5P.ContentType.getLibraryFilePath.
+    var nativeSetLinkAttribute = HTMLLinkElement.prototype.setAttribute;
+    HTMLLinkElement.prototype.setAttribute = function (name, value) {
+        if (name === 'href') {
+            if (value.startsWith('./libraries/')) {
+                const file = value.substr(12);
+                if (furtherH5PInlineResources[file]) {
+                    value = furtherH5PInlineResources[file];
+                }
             }
         }
-    }
-    nativeSetLinkAttribute.call(this, name, value);
-};
-Object.defineProperty(HTMLLinkElement.prototype, 'src', {
-    // also apply the modification if the src is set with the src property
-    set(value) {
-        this.setAttribute('src', value);
-    }
-});
+        nativeSetLinkAttribute.call(this, name, value);
+    };
+    Object.defineProperty(HTMLLinkElement.prototype, 'src', {
+        // also apply the modification if the src is set with the src property
+        set(value) {
+            this.setAttribute('src', value);
+        }
+    });
+})();
