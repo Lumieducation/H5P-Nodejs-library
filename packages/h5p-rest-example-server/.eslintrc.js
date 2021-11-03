cefc2f253a2ab7e20378d469290c7a172ec0e549
Sebastian Rettig <sr258@users.noreply.github.com>
2021-11-03 10:01:55 +0100

    refactor: enable go to definition between packages (#1853)
    * refactor: enable go to definition between packages
    
    * refactor: added VS Code launch config

--- packages/h5p-rest-example-server/.eslintrc.js
+++ packages/h5p-rest-example-server/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['../../.eslintrc.js'],
     parserOptions: {
-        project: 'tsconfig.json',
+        project: 'tsconfig.build.json',
         sourceType: 'module'
     },
     rules: {
