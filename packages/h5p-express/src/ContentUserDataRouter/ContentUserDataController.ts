56122b8f09eba8fb4a231d1fd1bf85412a503a91
JPSchellenberg <jps@Lumi.education>
2021-11-16 13:38:32 +0100

    feat(h5p-express): add ContentUserDataController

---
+++ packages/h5p-express/src/ContentUserDataRouter/ContentUserDataController.ts
@@ -0,0 +1,59 @@
+import * as express from 'express';
+
+import {
+    ContentUserDataManager,
+    IPostContentUserData
+} from '@lumieducation/h5p-server';
+import { IRequestWithUser } from '../expressTypes';
+
+interface IPostContentUserDataRequest
+    extends IPostContentUserData,
+        IRequestWithUser {}
+
+export default class ContentUserDataController {
+    constructor(protected contentUserDataManager: ContentUserDataManager) {}
+
+    /**
+     * Returns the userState for given contentId, dataType and subContentId
+     */
+    public getContentUserData = async (
+        req: IRequestWithUser,
+        res: express.Response<{
+            data: string;
+            success: boolean;
+        }>
+    ): Promise<void> => {
+        const { contentId, dataType, subContentId } = req.params;
+        const userState = await this.contentUserDataManager.loadContentUserData(
+            contentId,
+            dataType,
+            subContentId,
+            req.user
+        );
+        res.status(200).json({ data: userState, success: true });
+    };
+
+    /**
+     * Saving a userState for given contentId, datType and subContentId
+     *
+     */
+    public postContentUserData = async (
+        req: IPostContentUserDataRequest,
+        res: express.Response
+    ): Promise<void> => {
+        const { contentId, dataType, subContentId } = req.params;
+        const { user, body } = req;
+
+        await this.contentUserDataManager.saveContentUserData(
+            contentId,
+            dataType,
+            subContentId,
+            body.data,
+            body.invalidate,
+            body.preload,
+            user
+        );
+
+        res.status(200).end();
+    };
+}
