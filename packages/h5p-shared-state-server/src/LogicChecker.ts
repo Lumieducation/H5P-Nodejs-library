fff5bfb069eeebb523d85dd5beeb3811d38ae16b
Sebastian Rettig <serettig@posteo.de>
2022-01-04 10:44:45 +0100

    refactor: simplified logic checker structure

--- packages/h5p-shared-state-server/src/LogicChecker.ts
+++ packages/h5p-shared-state-server/src/LogicChecker.ts
@@ -1,139 +1,146 @@
 import { JSONPath } from 'jsonpath-plus';
 import { ILogicCheck } from './types';
 
-export default class LogicChecker {
-    check(params: any, data: any, checks: ILogicCheck[]): boolean {
-        const obj = { params, data };
-        return checks.every((c) => this.evaluateLogicCheck(c, obj));
+/**
+ * Compares two entities for equality using == and also checking arrays.
+ *
+ * If a and b are both arrays, their entries are compared (shallow compare).
+ * Equality means same value for everything expect object and same reference
+ * for objects.
+ */
+const compareEquality = (a: any | any[], b: any | any[]): boolean => {
+    if (Array.isArray(a) && Array.isArray(b)) {
+        if (a == null || b == null) return false;
+        if (a.length !== b.length) return false;
+        for (let i = 0; i < a.length; ++i) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
     }
+    return a == b;
+};
 
-    evaluateLogicCheck = (check: ILogicCheck, obj: any): boolean => {
-        return Object.keys(check).every((property) => {
-            if (!property.startsWith('$')) {
-                throw new Error(
-                    `Expression ${property} is not a JSON path or a query. It must start with $.`
-                );
+/**
+ * Recursively goes through checks and evaluates the object against them.
+ */
+const evaluateLogicCheckRec = (check: ILogicCheck, obj: any): boolean => {
+    return Object.keys(check).every((property) => {
+        if (!property.startsWith('$')) {
+            throw new Error(
+                `Expression ${property} is not a JSON path or a query. It must start with $.`
+            );
+        }
+        if (property === '$and') {
+            if (!Array.isArray(check[property])) {
+                throw new Error('$and requires an array');
             }
-            if (property === '$and') {
-                if (!Array.isArray(check[property])) {
-                    throw new Error('$and requires an array');
-                }
-                return (check[property] as ILogicCheck[]).every((c) =>
-                    this.evaluateLogicCheck(c, obj)
-                );
+            return (check[property] as ILogicCheck[]).every((c) =>
+                evaluateLogicCheckRec(c, obj)
+            );
+        }
+        if (property === '$or') {
+            if (!Array.isArray(check[property])) {
+                throw new Error('$or requires an array');
             }
-            if (property === '$or') {
-                if (!Array.isArray(check[property])) {
-                    throw new Error('$or requires an array');
-                }
-                return (check[property] as ILogicCheck[]).some((c) =>
-                    this.evaluateLogicCheck(c, obj)
-                );
-            }
-            if (property === '$not') {
-                return this.evaluateLogicCheck(check[property], obj);
-            }
-            if (property === '$nor') {
-                if (
-                    !Array.isArray(
-                        check[property] || check[property].length !== 2
-                    )
-                ) {
-                    throw new Error('$nor requires an array with two entry');
-                }
-                return (
-                    this.evaluateLogicCheck(check[property][0], obj) &&
-                    !this.evaluateLogicCheck(check[property][1], obj)
-                );
-            }
-            const evaluatedPath = JSONPath({
-                path: property,
-                json: obj,
-                preventEval: true,
-                wrap: false
-            });
-            const secondExpression = check[property];
+            return (check[property] as ILogicCheck[]).some((c) =>
+                evaluateLogicCheckRec(c, obj)
+            );
+        }
+        if (property === '$not') {
+            return evaluateLogicCheckRec(check[property], obj);
+        }
+        if (property === '$nor') {
             if (
-                typeof secondExpression === 'boolean' ||
-                typeof secondExpression === 'string' ||
-                typeof secondExpression === 'number' ||
-                Array.isArray(secondExpression)
+                !Array.isArray(check[property] || check[property].length !== 2)
             ) {
-                return this.compareEquality(evaluatedPath, secondExpression);
-            } else if (typeof secondExpression === 'object') {
-                if (Object.keys(secondExpression).length !== 1) {
-                    throw new Error('Error in query: empty object');
-                }
+                throw new Error('$nor requires an array with two entry');
+            }
+            return (
+                evaluateLogicCheckRec(check[property][0], obj) &&
+                !evaluateLogicCheckRec(check[property][1], obj)
+            );
+        }
+        const evaluatedPath = JSONPath({
+            path: property,
+            json: obj,
+            preventEval: true,
+            wrap: false
+        });
+        const secondExpression = check[property];
+        if (
+            typeof secondExpression === 'boolean' ||
+            typeof secondExpression === 'string' ||
+            typeof secondExpression === 'number' ||
+            Array.isArray(secondExpression)
+        ) {
+            return compareEquality(evaluatedPath, secondExpression);
+        } else if (typeof secondExpression === 'object') {
+            if (Object.keys(secondExpression).length !== 1) {
+                throw new Error('Error in query: empty object');
+            }
 
-                const secondExpressionValue =
-                    Object.values(secondExpression)[0];
-                let evaluatedSecondExpression;
-                if (
-                    typeof secondExpressionValue === 'boolean' ||
-                    typeof secondExpressionValue === 'string' ||
-                    typeof secondExpressionValue === 'number' ||
-                    Array.isArray(secondExpressionValue)
-                ) {
-                    evaluatedSecondExpression = secondExpressionValue;
-                } else if (typeof secondExpressionValue === 'object') {
-                    evaluatedSecondExpression = JSONPath({
-                        path: (secondExpressionValue as any).$query,
-                        json: obj,
-                        preventEval: true,
-                        wrap: false
-                    });
-                } else {
-                    throw new Error('Unknown type in query');
-                }
-                switch (Object.keys(secondExpression)[0]) {
-                    case '$eq':
-                        return this.compareEquality(
-                            evaluatedPath,
-                            evaluatedSecondExpression
-                        );
-                    case '$gt':
-                        return evaluatedPath > evaluatedSecondExpression;
-                    case '$gte':
-                        return evaluatedPath >= evaluatedSecondExpression;
-                    case '$in':
-                        if (!Array.isArray(evaluatedSecondExpression)) {
-                            return false;
-                        }
-                        return (evaluatedSecondExpression as any[]).includes(
-                            evaluatedPath
-                        );
-                    case '$lt':
-                        return evaluatedPath < evaluatedSecondExpression;
-                    case '$lte':
-                        return evaluatedPath <= evaluatedSecondExpression;
-                    case '$ne':
-                        return !this.compareEquality(
-                            evaluatedPath,
-                            evaluatedSecondExpression
-                        );
-                    case '$nin':
-                        if (!Array.isArray(evaluatedSecondExpression)) {
-                            return false;
-                        }
-                        return !(evaluatedSecondExpression as any[]).includes(
-                            evaluatedPath
-                        );
-                }
+            const secondExpressionValue = Object.values(secondExpression)[0];
+            let evaluatedSecondExpression;
+            if (
+                typeof secondExpressionValue === 'boolean' ||
+                typeof secondExpressionValue === 'string' ||
+                typeof secondExpressionValue === 'number' ||
+                Array.isArray(secondExpressionValue)
+            ) {
+                evaluatedSecondExpression = secondExpressionValue;
+            } else if (typeof secondExpressionValue === 'object') {
+                evaluatedSecondExpression = JSONPath({
+                    path: (secondExpressionValue as any).$query,
+                    json: obj,
+                    preventEval: true,
+                    wrap: false
+                });
             } else {
                 throw new Error('Unknown type in query');
             }
-        });
-    };
-
-    compareEquality = (a: any | any[], b: any | any[]): boolean => {
-        if (Array.isArray(a) && Array.isArray(b)) {
-            if (a == null || b == null) return false;
-            if (a.length !== b.length) return false;
-            for (let i = 0; i < a.length; ++i) {
-                if (a[i] !== b[i]) return false;
+            switch (Object.keys(secondExpression)[0]) {
+                case '$eq':
+                    return compareEquality(
+                        evaluatedPath,
+                        evaluatedSecondExpression
+                    );
+                case '$gt':
+                    return evaluatedPath > evaluatedSecondExpression;
+                case '$gte':
+                    return evaluatedPath >= evaluatedSecondExpression;
+                case '$in':
+                    if (!Array.isArray(evaluatedSecondExpression)) {
+                        return false;
+                    }
+                    return (evaluatedSecondExpression as any[]).includes(
+                        evaluatedPath
+                    );
+                case '$lt':
+                    return evaluatedPath < evaluatedSecondExpression;
+                case '$lte':
+                    return evaluatedPath <= evaluatedSecondExpression;
+                case '$ne':
+                    return !compareEquality(
+                        evaluatedPath,
+                        evaluatedSecondExpression
+                    );
+                case '$nin':
+                    if (!Array.isArray(evaluatedSecondExpression)) {
+                        return false;
+                    }
+                    return !(evaluatedSecondExpression as any[]).includes(
+                        evaluatedPath
+                    );
             }
-            return true;
+        } else {
+            throw new Error('Unknown type in query');
         }
-        return a == b;
-    };
+    });
+};
+
+/**
+ * Check if all the checks apply to the object.
+ */
+export function checkLogic(obj: any, checks: ILogicCheck[]): boolean {
+    return checks.every((c) => evaluateLogicCheckRec(c, obj));
 }
--- packages/h5p-shared-state-server/test/LogicChecker.test.ts
+++ packages/h5p-shared-state-server/test/LogicChecker.test.ts
@@ -1,36 +1,35 @@
-import LogicChecker from '../src/LogicChecker';
+import { checkLogic } from '../src/LogicChecker';
 
 describe('logic checker', () => {
-    const params = {
-        options: [
-            {
-                id: 'opt1',
-                text: 'Option 1'
-            },
-            {
-                id: 'opt2',
-                text: 'Option 2'
-            },
-            {
-                id: 'opt3',
-                text: 'Option 3'
+    const obj = {
+        params: {
+            options: [
+                {
+                    id: 'opt1',
+                    text: 'Option 1'
+                },
+                {
+                    id: 'opt2',
+                    text: 'Option 2'
+                },
+                {
+                    id: 'opt3',
+                    text: 'Option 3'
+                }
+            ]
+        },
+        data: {
+            votes: {
+                opt1: 1,
+                opt2: 2,
+                opt3: 3
             }
-        ]
-    };
-
-    const snapshot = {
-        votes: {
-            opt1: 1,
-            opt2: 2,
-            opt3: 3
         }
     };
 
-    const logicChecker = new LogicChecker();
-
     it('evaluates $eq', () => {
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[*].id': {
                         $eq: {
@@ -41,7 +40,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[*].id': {
                         $eq: ['opt1', 'opt2', 'opt3']
@@ -53,21 +52,21 @@ describe('logic checker', () => {
 
     it('evaluates implicit equality', () => {
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[0].id': 'opt1'
                 }
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[0].id': 'opt2'
                 }
             ])
         ).toEqual(false);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[*].id': ['opt1', 'opt2', 'opt3']
                 }
@@ -77,7 +76,7 @@ describe('logic checker', () => {
 
     it('evaluates $neq', () => {
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[*].text': {
                         $ne: {
@@ -88,7 +87,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[0].id': {
                         $ne: 'opt2'
@@ -100,7 +99,7 @@ describe('logic checker', () => {
 
     it('evaluates $gt', () => {
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options.length': {
                         $gt: 1
@@ -109,7 +108,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options.length': {
                         $gt: 5
@@ -118,7 +117,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(false);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.data.votes.opt2': {
                         $gt: 1
@@ -130,7 +129,7 @@ describe('logic checker', () => {
 
     it('evaluates $gte', () => {
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options.length': {
                         $gte: 3
@@ -139,7 +138,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options.length': {
                         $gte: 4
@@ -151,7 +150,7 @@ describe('logic checker', () => {
 
     it('evaluates $in', () => {
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[0].id': {
                         $in: { $query: '$.data.votes[*]~' }
@@ -160,7 +159,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[0].id': {
                         $in: ['opt1', 'opt2', 'opt3']
@@ -169,7 +168,7 @@ describe('logic checker', () => {
             ])
         ).toEqual(true);
         expect(
-            logicChecker.check(params, snapshot, [
+            checkLogic(obj, [
                 {
                     '$.params.options[0].id': {
                         $in: ['opt2', 'opt3']
