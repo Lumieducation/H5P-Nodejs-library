a63488bc7d559b16da367c3c8732319b5d6f2e4e
Sebastian Rettig <serettig@posteo.de>
2022-02-18 16:50:13 +0100

    refactor: downgraded editor core reference

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 # If the editor and core files are missing, we download them from GitHub.
 if [ ! -d "packages/h5p-examples/h5p/editor" ] || [ ! -d "packages/h5p-examples/h5p/core" ]
 then    
-    sh packages/h5p-examples/download-core.sh c79f97a16fd8c6fc0232c10d5bed5b94502ee9e9 70dc2da2f0702ec4a2483bfb7cbdacf9e843389d
+    sh packages/h5p-examples/download-core.sh c79f97a16fd8c6fc0232c10d5bed5b94502ee9e9 c886fa6ded498bbe0148e9484f9b1534facc264e
 else
     echo "Not downloading H5P Core and Editor files as they are already present!"
 fi
