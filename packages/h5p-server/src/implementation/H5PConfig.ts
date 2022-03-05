add90d4232bb3c252250ef5c06c2dad63bf5f106
Sebastian Rettig <serettig@posteo.de>
2022-03-05 10:46:14 +0100

    fix(h5p-server): added gltf and glb to allowed content file formats

--- packages/h5p-server/src/implementation/H5PConfig.ts
+++ packages/h5p-server/src/implementation/H5PConfig.ts
@@ -28,7 +28,7 @@ export default class H5PConfig implements IH5PConfig {
     public contentHubMetadataRefreshInterval: number = 1 * 1000 * 60 * 60 * 24;
     public contentUserDataUrl: string = '/contentUserData';
     public contentWhitelist: string =
-        'json png jpg jpeg gif bmp tif tiff svg eot ttf woff woff2 otf webm mp4 ogg mp3 m4a wav txt pdf rtf doc docx xls xlsx ppt pptx odt ods odp xml csv diff patch swf md textile vtt webvtt';
+        'json png jpg jpeg gif bmp tif tiff svg eot ttf woff woff2 otf webm mp4 ogg mp3 m4a wav txt pdf rtf doc docx xls xlsx ppt pptx odt ods odp xml csv diff patch swf md textile vtt webvtt gltf glb';
     public coreApiVersion: { major: number; minor: number } = {
         major: 1,
         minor: 24
