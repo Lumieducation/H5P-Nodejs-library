8a61d656a739a02f49d86ddaadcb6d5fb63c7f45
Sebastian Rettig <serettig@posteo.de>
2022-04-18 16:30:02 +0200

    refactor: refix tests

--- packages/h5p-express/test/H5PAjaxExpressRouter.test.ts
+++ packages/h5p-express/test/H5PAjaxExpressRouter.test.ts
@@ -446,7 +446,9 @@ describe('Express Ajax endpoint adapter', () => {
             .reply(() => [
                 200,
                 fsExtra.createReadStream(
-                    path.resolve('test/data/example-packages/H5P.DragText.h5p')
+                    path.resolve(
+                        'packages/h5p-server/test/data/example-packages/H5P.DragText.h5p'
+                    )
                 )
             ]);
         const result = await supertest(app).post(
