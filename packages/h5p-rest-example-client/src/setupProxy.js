fd26236d43a0b3f8e0c4ec300c2b4c5b040855a8
Sebastian Rettig <serettig@posteo.de>
2025-01-18 09:58:16 +0100

    refactor(rest-example): added base path to target of proxy-middleware

--- packages/h5p-rest-example-client/src/setupProxy.js
+++ packages/h5p-rest-example-client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/h5p',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8080',
+      target: 'http://127.0.0.1:8080/h5p',
       changeOrigin: true,
     })
   );
