e18d34bbd129cc69a75a775e0abcd72d2b28e76e
Sebastian Rettig <serettig@posteo.de>
2025-03-02 20:17:15 +0100

    refactor: make jest use tsconfig.build.json

--- jest.config.js
+++ jest.config.js
@@ -163,7 +163,7 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.tsx?$': 'ts-jest'
+        '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.build.json' }]
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
