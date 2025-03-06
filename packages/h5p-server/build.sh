b2aa7aeecb4033f446a98519e770fb5fd5982d8b
Sebastian Rettig <serettig@posteo.de>
2025-03-06 19:59:24 +0100

    refactor: h5p-server as hybrid

--- packages/h5p-server/build.sh
+++ packages/h5p-server/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 rm -rf build
-npx tsc -p ./tsconfig.build.json
-cp -r src/schemas build/src/schemas
-cp -r assets build
+npx tsc -p ./tsconfig.cjs.json
+npx tsc -p ./tsconfig.esm+types.json
+cp -r src/schemas build/esm/src/schemas
+cp -r src/schemas build/cjs/src/schemas
+cp -r assets build/cjs
+cp -r assets build/esm
+npx tsc-esm-fix --src='./build/esm/src/**/*.js' --ext='.js' --tsconfig='./tsconfig.esm+types.json'
+echo '{"type": "commonjs"}' > ./build/cjs/package.json && echo '{"type": "module"}' > ./build/esm/package.json
