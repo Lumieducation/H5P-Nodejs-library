0ce079b9a1139a80e0252089b522f56af5b0db47
Sebastian Rettig <serettig@posteo.de>
2025-08-17 12:31:19 +0200

    test: removed broken e2e tests

--- packages/h5p-examples/jest.e2e.config.js
+++
@@ -1,9 +0,0 @@
-const base = require('./jest.config');
-
-module.exports = {
-    ...base,
-
-    // The glob patterns Jest uses to detect test files
-    testMatch: ['**/test/e2e/**/*.test.ts'],
-    testPathIgnorePatterns: []
-};
