65660fceb264197aa39ba1c8c866482d2bdaf128
Sebastian Rettig <serettig@posteo.de>
2026-02-22 17:28:27 +0100

    refactor(rest example client): replaced react-scripts with vite

---
+++ packages/h5p-rest-example-client/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            '/h5p': {
+                target: 'http://127.0.0.1:8080',
+                changeOrigin: true
+            },
+            '/login': {
+                target: 'http://127.0.0.1:8080',
+                changeOrigin: true
+            },
+            '/logout': {
+                target: 'http://127.0.0.1:8080',
+                changeOrigin: true
+            }
+        }
+    },
+    build: {
+        outDir: 'build'
+    }
+});
